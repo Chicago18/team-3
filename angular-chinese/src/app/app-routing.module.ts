c5d538d0f3cec9caa22987909685c3f54dbbbdcd
Oksana Sakhniuk <osakhniu@hawk.iit.edu>
2018-10-13 00:41:35 -0500

    Added students endpoint

--- angular-chinese/src/app/app-routing.module.ts
+++ angular-chinese/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+//import { DashboardComponent }   from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	// { path: 'dashboard', component: DashboardComponent },
+	//{ path: 'detail/:id', component: StudentDetailComponent },
+	{ path: 'students', component: StudentsComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
